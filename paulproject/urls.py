682081d3825cc314ffabcfa836278aee009bc98e
FloresJp <jhonpaulflores704@gmail.com>
2025-12-10 07:14:32 +0800

    Initial commit: fixed flattened structure

--- paulproject/urls.py
+++ paulproject/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('paulapp.urls')),  
+    path('', include('paulapp.urls')),  # include your app URLs
 ]
 
